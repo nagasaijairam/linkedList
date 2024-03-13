dceb692f416c86642825f1a39a32d5dba8c57542
nagasaijairam <nagasaijairam@gmail.com>
2024-03-13 22:42:46 +0530

    TESTE

--- Linked List/LinkedList.py
+++ Linked List/LinkedList.py
@@ -5,19 +5,4 @@ class Node:
 
 Head=Node(1)
 
-# def insert(l):
-#     Head=None 
-#     temp=Head 
-
-#     for i in l:
-#         if Head == None: 
-#             Head=Node(i)
-
-#         else:
-#             temp.next=Node(i)
-#             temp=temp.next 
-#     return Head 
-
-# ans=insert([1,2,3,4,5,6])
-# print   (ans)         
-
+return Head
