dcbdc1c8eef40187f62d9b6eec2f21844c8380c7
nagasaijairam <nagasaijairam@gmail.com>
2024-03-19 21:43:53 +0530

    anothe commit

--- Linked List/LinkedList.py
+++ Linked List/LinkedList.py
@@ -18,25 +18,51 @@ def traverse(Head):
         temp=temp.next
     print()
 
-def insertAtHead(x,Head):
-    if Head[0]==None:
-        insertAtHead(x,Head)
+def insertAtHead(Head,x):
+ 
     newHead=Node(x)
     newHead.next=Head[0]
     Head[0]=newHead    
 
 
-def insertAtEnd(x,Head):
+def insertAtEnd(Head,x):
     newNode=Node(x)
     temp=Head[0]
     while temp.next!=None:
         temp=temp.next 
     temp.next=newNode         
+
+def insertAtMiddle(Head,x,k):
+    if k==1:
+        insertAtHead(Head,x)
+        return 
+    temp = Head[0]
+    count=1 
+    while  count<k-1:
+        temp=temp.next 
+        count+=1 
+    newNode=Node(x)
+    newNode.next=temp.next
+    temp.next=newNode
+
+def deleteAtStart(Head):
+    if Head[0] is not None:
+        temp=Head[0] 
+        Head[0]=temp.next 
+
+
              
 
 
-traverse(Head) #1->2->
-insertAtHead(0,Head)#0->1->2->
-insertAtEnd(3,Head) #0->1->2->3->
-insertAtEnd(4,Head) #0->1->2->3->4->
+# traverse(Head) #1->2->
+insertAtHead(Head,0)#0->1->2->
+insertAtEnd(Head,3) #0->1->2->3->
+# insertAtEnd(Head,4) #0->1->2->3->4->
+# insertAtMiddle(Head,1.5,3)
+# deleteAtStart(Head)
 traverse(Head) #0->1->2->3->4->
+print(Head[0].val)
+temp=Head[0]
+while temp:
+    print(temp.val)
+    temp=temp.next
